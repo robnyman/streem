9fab0f71bd34550c0d8ef903f96844441f6ca9fa
Robert Nyman <robert@robertnyman.com>
2014-12-19 16:01:45 +0100

    Minor formatting changes

--- js/base.js
+++ js/base.js
@@ -4,11 +4,10 @@ addEventListener('core-header-transform', function(e) {
     var d = e.detail;
     var m = d.height - d.condensedHeight;
     var scale = Math.max(0.75, (m - d.y) / (m / 0.25)  + 0.75);
-    titleStyle.transform = titleStyle.webkitTransform =
-    'scale(' + scale + ') translateZ(0)';
+    titleStyle.transform = titleStyle.webkitTransform = 'scale(' + scale + ') translateZ(0)';
 });
 
-// Tab navigation - alternate version if <template is="auto-binding"> isn't used
+// Tab navigation - alternate version if <template is="auto-binding"> and/or routing isn't used
 /*
 var mainNav = document.querySelector("#main-nav"),
     contentPages = document.querySelector("#content-pages");
@@ -22,8 +21,8 @@ if (mainNav && contentPages) {
 // Quotes navigation - takes core-animated-pages first transition into account
 setTimeout(function () {
     var quotes = document.querySelectorAll(".quote-wrapper"),
-    quotesNav = document.querySelector("#quotes-nav"),
-    quotesNavSelectedIndex = 0;
+        quotesNav = document.querySelector("#quotes-nav"),
+        quotesNavSelectedIndex = 0;
 
     if (quotesNav) {
         quotesNav.addEventListener("click", function () {
@@ -32,13 +31,20 @@ setTimeout(function () {
             quotes[quotesNavSelectedIndex].classList.add("selected");
         });
     }
-}, 1000);
+}, 500);
 
 // Get page template reference
 var template = document.querySelector('template[is="auto-binding"]');
 
-// Add start default route
 if (template) {
+    // Add page data
+    template.pages = [
+        {name: "Virtual water footprint", hash: "home"},
+        {name: "Water types & cost", hash: "water"},
+        {name: "Help organizations", hash: "help-organizations"}
+    ];
+
+    // Add start default route
     template.addEventListener("template-bound", function(e) {
         // Use URL hash for initial route. Otherwise, use the first page.
         this.route = this.route || "home";
